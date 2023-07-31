596fc7839fc29096f06d8da7588dd8c34963f5be
Rabisko0 <rodrigocsw@gmail.com>
2023-07-30 21:43:25 -0300

    funcion "winner" created

--- js/script.js
+++ js/script.js
@@ -10,11 +10,32 @@ function getPlayerChoice(){
 
 function getComputerChoice(){
     let ramdomSelection = Math.floor(Math.random() * options.length);
-    return computerChoice = options.ramdomSelection;
+    return computerChoice = options[ramdomSelection];
 }
 
-function winner (computerChoice, playerChoice){
+function winner (){
+    
+    getComputerChoice();
+    getPlayerChoice();
+
+    console.log(`Computador escolheu ${computerChoice}.`);
+    console.log(`Jogador escolheu ${playerChoice}.`);
+
     if (computerChoice == playerChoice){
+         return console.log('Foi empate.')
+    }
+    else if ((computerChoice == "pedra" && playerChoice == "tesoura") || (computerChoice == "papel" && playerChoice == "pedra") || (computerChoice == "papel" && playerChoice == "pedra")){
+        return console.log('Computador venceu.');
+    }
+    else{
+        return console.log('Jogador venceu.')
+    }
+}
+
+function fullGame(){
+    for(let i = 0; i < 5; i++){
+        
+        winner();
         
     }
 }
